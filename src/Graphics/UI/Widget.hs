310663851dd1c0419e9d75a44656fc2e6d8bacdd
myuon <ioi.joi.koi.loi@gmail.com>
2017-10-20 23:35:32 +0900

    Graphics.UI.Widget

---
+++ src/Graphics/UI/Widget.hs
@@ -0,0 +1,12 @@
+module Graphics.UI.Widget
+  (
+  -- * Core
+    module Graphics.UI.Widget.Core
+
+  -- * Interface
+  , module Graphics.UI.Widget.Class
+  ) where
+
+import Graphics.UI.Widget.Core
+import Graphics.UI.Widget.Class
+
